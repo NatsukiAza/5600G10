c298d2b60566fac3fa3328f2f9ef1cf045dcca77
Adr1z <97919020+Adr1z@users.noreply.github.com>
2025-11-01 19:39:16 -0300

    Update ImportarInquilinos_Propietarios procedure
    Modified the ImportarInquilinos_Propietarios stored procedure to change data types for Documento and Telefono to VARCHAR. Updated the data handling logic to include trimming and type casting for CSV import.

--- SP ImportarInquilinos_Propietarios.sql
+++ SP ImportarInquilinos_Propietarios.sql
@@ -1,12 +1,19 @@
-CREATE PROCEDURE ImportarCSV
+USE COM025600
+
+IF OBJECT_ID('dbo.ImportarInquilinos_Propietarios', 'P') IS NOT NULL
+    DROP PROCEDURE dbo.ImportarInquilinos_Propietarios;
+GO
+CREATE PROCEDURE ImportarInquilinos_Propietarios
 	@RutaArchivoNovedades VARCHAR(500)
 AS BEGIN
     CREATE TABLE #DatosImportadosCSV(
         Nombre VARCHAR(100),
         Apellido VARCHAR(100),
-        Documento INT,
+        Documento VARCHAR(100),
         Email_Personal VARCHAR(100),
-        Telefono INT,
+        Telefono VARCHAR(50),           -- Ahora VARCHAR
+        CBU_CVU_Pago VARCHAR(50),
+        Inquilino_Flag VARCHAR(10)
     );
     --
     DECLARE @ComandoSQL NVARCHAR(MAX);
@@ -19,40 +26,55 @@ AS BEGIN
             FIRSTROW = 2
         )';
     --
-    BEGIN TRY
-        EXEC sp_executesql @ComandoSQL;
-    END TRY
-    BEGIN CATCH
-        PRINT 'ERROR AL EJECUTAR BULK INSERT. Revise la ruta del archivo y los permisos.';
-        RETURN;
-    END CATCH;
+    EXEC sp_executesql @ComandoSQL;
     --
-    MERGE Persona AS Transformado
-    USING (
-        SELECT DISTINCT
-            1 AS Tipo_Documento, -- Se inserta la constante 1
-            DatoImportado.Documento AS Numero_Documento,
+    WITH FuenteCSV AS (
+        SELECT
+            1 AS Tipo_Documento,
+            TRY_CAST((TRIM(DatoImportado.Documento)) AS INT) AS Numero_Documento,
             TRIM(DatoImportado.Nombre) AS Nombre, -- Limpieza de espacios
             TRIM(DatoImportado.Apellido) AS Apellido, -- Limpieza de espacios
             TRIM(DatoImportado.Email_Personal) AS Correo_Electronico, -- Limpieza de espacios
-            DatoImportado.Telefono AS Telefono
-        FROM #DatosImportadosCSV AS DatoImportado
-    ) AS Fuente 
+            TRY_CAST((TRIM(DatoImportado.Telefono)) AS INT) AS Telefono,
+            ROW_NUMBER() OVER (PARTITION BY DatoImportado.Documento ORDER BY DatoImportado.Documento) AS RN
+        FROM 
+            #DatosImportadosCSV AS DatoImportado
+        WHERE
+            NULLIF(TRIM(DatoImportado.Documento),'') IS NOT NULL 
+            AND NULLIF(TRIM(DatoImportado.Nombre), '') IS NOT NULL
+            AND NULLIF(TRIM(DatoImportado.Apellido), '') IS NOT NULL
+            AND NULLIF(TRIM(DatoImportado.Email_Personal), '') IS NOT NULL
+            AND NULLIF(TRIM(DatoImportado.Telefono), '') IS NOT NULL
+    )
+    MERGE dbo.Persona AS Transformado
+    USING (
+        SELECT 
+            Tipo_Documento, 
+            Numero_Documento, 
+            Nombre, 
+            Apellido, 
+            Correo_Electronico, 
+            Telefono
+        FROM 
+            FuenteCSV
+        WHERE 
+            RN = 1
+    ) AS Fuente
     ON (
         Transformado.numero_documento = Fuente.Numero_Documento AND Transformado.tipo_documento = Fuente.Tipo_Documento
     )
     --
     WHEN MATCHED THEN
         UPDATE SET
-            Transformado.nombre = Fuente.Nombre,                 -- Se actualiza el nombre por si hubo corrección
-            Transformado.apellido = Fuente.Apellido,             -- Se actualiza el apellido
+            Transformado.nombre = Fuente.Nombre,
+            Transformado.apellido = Fuente.Apellido,
             Transformado.correo_electronico = Fuente.Correo_Electronico,
             Transformado.Telefono = Fuente.Telefono
     --
     WHEN NOT MATCHED BY TARGET THEN
         INSERT (tipo_documento, numero_documento, nombre, apellido, correo_electronico, Telefono)
         VALUES (
-            Fuente.Tipo_Documento, 
+            Fuente.Tipo_Documento, -- Se inserta la constante 1
             Fuente.Numero_Documento,
             Fuente.Nombre,
             Fuente.Apellido,
@@ -63,3 +85,11 @@ AS BEGIN
     IF OBJECT_ID('tempdb..#DatosImportadosCSV') IS NOT NULL
     DROP TABLE #DatosImportadosCSV;
 END
+GO
+--
+DECLARE @Ruta VARCHAR(500) = 'C:\temp\Inquilino-propietarios-datos.csv'
+EXEC ImportarInquilinos_Propietarios @RutaArchivoNovedades = @Ruta
+--
+GO
+
+SELECT * FROM dbo.Persona
